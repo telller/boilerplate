1623309ed0e02c4e9cb8a762f096f630e31977a4
Vlad Golyachenko <vlad.golyachenko@zazmic.com>
2020-03-25 20:19:23 +0200

    chat wip

--- server.js
+++ server.js
@@ -1,19 +1,61 @@
+import { MongoClient } from 'mongodb'
+import bodyParser from 'body-parser'
+import socketIO from 'socket.io'
 import express from 'express'
 import http from 'http'
 import path from 'path'
 import './config'
 
 const appDir = path.resolve(__dirname, './')
-const PORT = process.env.PORT || 3000
 const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
+let db
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(appDir))
+app.use(bodyParser.json())
+
+io.on('connection', socket => {
+  socket.broadcast.emit('broadcast', { online: io.engine.clientsCount, message: 'connected' })
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('broadcast', { online: io.engine.clientsCount, message: 'disconnect' })
+  })
+})
+
+app.get('/users', (req, res) => {
+  db.collection('users').find({}).toArray((err, docsres) => {
+    if (err) {
+      console.log(err)
+      return res.sendStatus(500)
+    } else {
+      res.send(docsres)
+    }
+  })
+})
+
+app.post('/users', (req, res) => {
+  console.log({ body: req.body })
+  db.collection('users').insert({ name: req.body.name }, err => {
+    if (err) {
+      console.log(err)
+      return res.sendStatus(500)
+    } else {
+      res.sendStatus(200)
+    }
+  })
+})
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(appDir, 'index.html'))
 })
 
-http.createServer(app).listen(PORT, () => {
-  console.log('Express server listening on port ' + PORT)
-  console.log('http://localhost:' + PORT)
+MongoClient.connect(process.env.MONGO_URL, (err, database) => {
+  if (err) return console.log(err)
+  db = database.db('boilerplate')
+  server.listen(process.env.PORT, () => {
+    console.log(`Express server listening on port ${process.env.PORT}`)
+    console.log(`http://localhost:${process.env.PORT}`)
+  })
 })
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,17 @@
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
-import React from 'react'
+import io from 'socket.io-client'
 import './index.styl'
 
 const Home = () => {
-  return (
-    <div id='home'>
-      Home page WORKING!
-    </div>
-  )
+  const socket = io(window.location.origin)
+
+  useEffect(() => {
+    socket.on('broadcast', (data) => console.log({ data }))
+  }, [])
+
+  return <div id='home'>Home page WORKING!</div>
 }
 
-const mapStateToProps = state => ({})
+const mapStateToProps = (state) => ({})
 export default connect(mapStateToProps)(Home)
