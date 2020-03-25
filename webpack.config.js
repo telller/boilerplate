a3facba653ece18a0ce6fbd7f88625c27646b69d
Vlad Golyachenko <vlad.golyachenko@zazmic.com>
2020-03-25 17:30:22 +0200

    upd webpack config and packages versions

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,12 @@ const webpack = require('webpack')
 const path = require('path')
 
 const alias = {
+  components: path.resolve('./src/components'),
+  services: path.resolve('./src/services'),
+  helpers: path.resolve('./src/helpers'),
+  store: path.resolve('./src/store'),
+  root: path.resolve('../'),
   'react-dom': '@hot-loader/react-dom',
-  'components': path.resolve('./src/components'),
-  'services': path.resolve('./src/services'),
-  'helpers': path.resolve('./src/helpers'),
-  'store': path.resolve('./src/store'),
-  'root': path.resolve('../'),
 }
 
 const getConfigVars = stage => {
@@ -37,67 +37,64 @@ const getConfigVars = stage => {
   return env
 }
 
-module.exports = env => {
-  const STAGE = env && env.STAGE
-  return ({
-    entry: ['babel-polyfill', './src/app.jsx'],
-    output: {
-      path: path.resolve(__dirname, './'),
-      filename: 'dist/bundle.js'
-    },
-    devtool: STAGE === 'prod' ? false : 'source-map',
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx?)$/,
-          exclude: [/node_modules/],
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                presets: [['env', {'modules': false}], 'stage-0', 'react']
-              }
-            }
-          ]
-        },
-        {
-          test: /\.styl$/,
-          use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: ['css-loader', 'stylus-loader']
-          }))
-        },
-        {
-          test: /\.css/,
-          use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: ['css-loader']
-          }))
-        },
-        {
-          test: /\.(img|png|svg)$/,
-          use: 'url-loader'
-        }
-      ]
-    },
-    devServer: {
-      historyApiFallback: true,
-      stats: {
-        version: false,
-        modules: false,
-        assets: false,
-        hash: false
+module.exports = env => ({
+  entry: ['@babel/polyfill', './src/app.jsx'],
+  output: {
+    path: path.resolve(__dirname, './'),
+    filename: 'dist/bundle.js',
+  },
+  devtool: env && env.STAGE === 'prod' ? false : 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx?)$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
       },
-      port: '3000'
-    },
-    plugins: [
-      new ExtractTextPlugin('dist/bundle.css'),
-      new webpack.NamedModulesPlugin(),
-      new webpack.DefinePlugin({ 'process.env': getConfigVars(STAGE) })
+      {
+        test: /\.styl$/,
+        use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'stylus-loader'],
+        })),
+      },
+      {
+        test: /\.css/,
+        use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader'],
+        })),
+      },
+      {
+        test: /\.(img|png|svg)$/,
+        use: 'url-loader',
+      },
     ],
-    resolve: {
-      extensions: ['.js', '.jsx'],
-      alias: alias
-    }
-  })
-}
+  },
+  devServer: {
+    historyApiFallback: true,
+    stats: {
+      entrypoints: false,
+      children: false,
+      builtAt: false,
+      version: false,
+      modules: false,
+      assets: false,
+      hash: false,
+    },
+    port: '3000',
+  },
+  plugins: [
+    new webpack.DefinePlugin({ 'process.env': getConfigVars(env && env.STAGE) }),
+    new ExtractTextPlugin({ filename: 'dist/bundle.css', allChunks: true }),
+    new webpack.NamedModulesPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: alias,
+  },
+})
